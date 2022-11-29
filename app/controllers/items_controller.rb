e2586d47684bd380ee7c5c196a3d6cc030e5f22d
Rhoda Gachimu <gachimurhoda@gmail.com>
2022-11-29 11:07:54 +0300

    completed

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,8 +1,33 @@
 class ItemsController < ApplicationController
-
+  rescue_from ActiveRecord::RecordNotFound, with: :record_not_found
   def index
-    items = Item.all
+    if params[:user_id]
+      user = User.find(params[:user_id])
+      items = user.items
+    else
+      items = Item.all
+    end
     render json: items, include: :user
   end
+  def show
+    item = Item.find(params[:id])
+    render json: item
+  end
 
+  def create
+    item = Item.new(item_params)
+    item.user_id = params[:user_id]
+    item.save
+    render json: item, status: :created
+  end
+
+  private
+
+  def record_not_found
+    render json: { error: "Record not found" }, status: :not_found
+  end
+
+  def item_params
+    params.permit(:name, :description, :price)
+  end
 end
